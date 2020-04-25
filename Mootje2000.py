9f112299cbfb34de5ea23c995f98f03e08abd520
AliHafeez337 <alihafeez337@gmail.com>
2020-04-25 16:30:09 +0500

    3rd - 2nd version of the script, perfectyly fine and very efficient...

--- Mootje2000.py
+++ Mootje2000.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[50]:
 
+import os 
+os.environ["LANG"] = "en_US.UTF-8"
 
 import pymongo
 client = pymongo.MongoClient("mongodb://mootje:mootje2000@82.217.36.166/properties") # defaults to port 27017
@@ -11,7 +13,7 @@ col=db.ids
 col_remaining=db.remaining_ids
 
 
-# In[4]:
+# In[62]:
 
 
 import json
@@ -21,22 +23,36 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('start-maximized')
+options.add_argument('disable-infobars')
+options.add_argument("--disable-extensions")
 
-chromedriver_location = "E:\Projects\project - company\Mootje2000\chromedriver"
+
+# In[78]:
+
+print("Please wait while we divide the ids...")
+
 
 remaining_ids=col_remaining.find()
 arr = []
 url = ''
 temp = ''
 loop = 0
+crawled = 0
 for i in remaining_ids:
 #     print('if runs')
     loop += 1
-    if i['ids']!= None:
+    if i['name']!= None:
         col_remaining.delete_one({"_id":i['_id']})
-        arr=i['ids'].copy()
+        
+        crawled = i['crawled']
         temp = i["name"]
         url=i['search_url']
+        start = i['start']
+        end = i["end"]
+        range1 = i["query"]
         
     break
         
@@ -46,77 +62,83 @@ if (loop == 0):
     list_=[]
     for i in docs:
         list_.append(i)
-#     print(list_[-1])
     s=list_[-1]
     start=s["starting_ids"]
-#     print(start)
     end=s["ending_ids"]
     url=s["search_url"]
     range1=s["query"]
+    temp = s["name"]
 
-    arr = []
-    idInput = ''
-    # range1 = 50
-    no = 0
-    inserted = 0
-    if (end > start):
-        rang = len(range(end-start))+1
-        for i in (range(rang)):
-            no = i
-            if (inserted >= range1):
-    #             print('naye',arr)
-                idInput = idInput[:-1]
+arr = []
+idInput = ''
+no = 0
+inserted = 0
+if (end > start):
+    rang = len(range(end-start))+1
+    for i in (range(rang)):
+        no = i
+        if (inserted >= range1):
+            idInput = idInput[:-1]
 
-#                 print(idInput)
-    #             str1 = ','.join(str(e) for e in arr)
-    #             print(str1)
-                arr.append(idInput)
-    #             print(arr)
-                idInput = ''
-                inserted = 0
-    #             print(arr, no+start, end)
-                if (no+start < end+1):
-                    idInput = idInput + str(no+start) + ','
-    #                 print(idInput)
-                    inserted += 1
-                    if(no+start == end):
-                        idInput = idInput[:-1]
+            arr.append(idInput)
+            idInput = ''
+            inserted = 0
 
-                        arr.append(idInput)
-                        idInput = ''
-                        inserted = 0
-    #                     print(arr, no+start, end)
-            else:
-    #             print('eys',arr)
+            if (no+start < end+1):
                 idInput = idInput + str(no+start) + ','
-    #             print(idInput)
+
                 inserted += 1
                 if(no+start == end):
                     idInput = idInput[:-1]
+
                     arr.append(idInput)
                     idInput = ''
                     inserted = 0
-#     print(arr)
-    temp = s["name"]
 
+        else:
 
+            idInput = idInput + str(no+start) + ','
 
-
-remaining=arr.copy()
+            inserted += 1
+            if(no+start == end):
+                idInput = idInput[:-1]
+                arr.append(idInput)
+                idInput = ''
+                inserted = 0
 
 colSave=db[temp]
 
+# print(crawled)
+if (loop > 0):
+    arr = arr[crawled:]
+
+
+# In[79]:
+
+
+# print(arr[-100::])
 # print(arr)
+
+
+# In[80]:
+
+
+# save starting, ending, query, url and name in remaining
+
 print("Starting crawling...")
-for i in arr:
+
+driver = webdriver.Chrome('/home/ali/Desktop/Scrapping/chromedriver', options = options)
+
+indexDone = 0
+for index, i in enumerate(arr):
+#     print(index)
 
     print("Crawling for ids: "+i)
     try: 
         
-#         if (arr.index(i) == 1):
+#         if (index == 2):
 #             print(1/0)
             
-        driver = webdriver.Chrome(chromedriver_location, options=options)
         driver.get(url)
         input_field='//*[@id="objectIds"]'
         driver.find_element_by_xpath(input_field).send_keys(i)
@@ -126,21 +148,23 @@ for i in arr:
         driver.find_element_by_xpath(get_json).click()
         txt=driver.find_element_by_xpath("/html/body/pre").text
         data=json.loads(txt)
+        
 #         print(type(data))
         colSave.insert_one(data)
+        indexDone += 1
         
 #         print(i)
-#         print(arr.index(i))
-        remaining.pop(0)
-        if (arr.index(i) > 0):
+#         print(index)
+
+        if (index > 0):
             #delete remaining
             try:
                 col_remaining.delete_one({"_id":1000})
             except:
                 pass
-        if (arr.index(i) < len(arr)):
+        if (index < len(arr)):
             #save remaining
-            remainig_save={ "_id": 1000, "ids":remaining,"search_url":url,"name":temp}
+            remainig_save={ "_id": 1000, "crawled": indexDone+crawled, "start": start, "end": end, "query": range1,"search_url":url,"name":temp}
             print("Progress Saved...")
             col_remaining.insert_one(remainig_save)
         
@@ -151,19 +175,21 @@ for i in arr:
             col_remaining.delete_one({"_id":1000})
         except:
             pass
-        remainig_save={"ids":remaining,"search_url":url,"name":temp}
-        print("We encountered an error, but we saved the remaining record to be crawled, please rerun the program...")
+        remainig_save={"start": start, "crawled": indexDone+crawled, "end": end, "query": range1, "name":temp, "search_url":url}
         col_remaining.insert_one(remainig_save)
+        print("We encountered an error, but we saved the remaining record to be crawled, please rerun the program...")
         break
 
 try:
     col_remaining.delete_one({"_id":1000})
 except:
     pass
+
+driver.close()
 print("Crawling done...")
 
 
-# In[ ]:
+# In[28]:
 
 
 
